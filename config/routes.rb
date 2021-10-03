bd1de73296c849ff135feed831b5b042a4b5e43c
N_hiromi <ryupi-20@outlook.jp>
2021-10-03 11:06:17 +0900

    commentsController作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'comments/index'
+  get 'comments/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
