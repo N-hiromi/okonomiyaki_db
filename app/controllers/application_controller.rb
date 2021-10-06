effbe9c3277582ca00c038dc1dfcc7cbfea9ec97
N_hiromi <ryupi-20@outlook.jp>
2021-10-06 10:48:59 +0900

    deviseでのsignin, signout, logoutの機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  #def after_sign_in_path_for(resource)
-   # flash[:notice] = "ログインに成功しました"
-    #tops_path
-  #end
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインに成功しました"
+    tops_path
+  end
 
-  #def after_sign_out_path_for(resource)
-   # flash[:alert] = "ログアウトしました"
-    #sign_out_path
-  #end
-
-def search
-  @q = Rental.ransack(params[:q])
-end
+  def after_sign_out_path_for(resource)
+    flash[:alert] = "ログアウトしました"
+    new_user_session_path
+  end
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :department])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :department])
     end
 
     
