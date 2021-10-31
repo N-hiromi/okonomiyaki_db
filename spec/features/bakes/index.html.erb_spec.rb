24e5faca17a390958dbdc3877c67b47149850e0a
N_hiromi <ryupi-20@outlook.jp>
2021-10-31 13:50:24 +0900

    検索機能のテストをテスト

--- spec/features/bakes/index.html.erb_spec.rb
+++ spec/features/bakes/index.html.erb_spec.rb
@@ -18,10 +18,20 @@ RSpec.describe "bakes/index.html.erb", type: :feature do
       expect(page).to have_content bake.updated_at.strftime('%Y/%m/%d')
     end
 
+    scenario 'can be searched bakes for name' do
+      #fill_in "bake_name", with: "Bake_name"
+      fill_in 'q[name_cont]', with: "Bake3"
+      find('input[name="commit"]').click
+      #binding.pry
+      #expect(page).to have_content bake.name
+      expect(page).to have_content "検索結果なし"
+    end
+
     scenario 'have link to bakes#show' do
       click_link bake.name, match: :first
       expect(page).to have_current_path bake_path(id: bake.id)
     end
+
 
   end
 end
